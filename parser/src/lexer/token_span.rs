bdf7f57bd47f873cc30face76bc5d30b5fa9ddf3
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-09-03 08:56:14 +0100

    Added StringKind, IdentifierKind, and Location/Locatable.

--- parser/src/lexer/token_span.rs
+++ parser/src/lexer/token_span.rs
@@ -1,6 +1,5 @@
-use crate::lexer::token_details::TokenDetails;
 use crate::lexer::LexerError::InvalidTokenRange;
-use crate::lexer::{DetailedError, Lexer};
+use crate::lexer::{Lexer, Locatable, LocatableError, Location};
 use std::ops::Range;
 
 pub struct TokenSpan<'lex, 'source> {
@@ -10,32 +9,29 @@ pub struct TokenSpan<'lex, 'source> {
 
 impl<'lex, 'source> TokenSpan<'lex, 'source> {
 
-    pub fn new(lexer: &'lex Lexer<'source>, start_pos: usize) -> Result<Self, DetailedError> {
+    pub fn new(lexer: &'lex Lexer<'source>, start_pos: usize) -> Result<Self, LocatableError> {
 
         let end_pos = lexer.buffer.current_index();
         let range = start_pos..end_pos;
 
         if start_pos > end_pos || end_pos > lexer.buffer.source().len() {
-            let details = TokenDetails::new(range, lexer.buffer.location());
-            return Err((InvalidTokenRange, details))
+            let (line, col) = lexer.buffer.location();
+            let details = Location::new(range, line, col);
+            return Err(Locatable::new(InvalidTokenRange, details))
         }
 
         Ok(Self { lexer, range })
     }
 
     #[inline]
-    pub fn details(&self) -> TokenDetails {
-        TokenDetails::new(self.range.clone(), self.location())
+    pub fn location(&self) -> Location {
+        let (line, col) = self.lexer.buffer.location_at(self.range.start);
+        Location::new(self.range.clone(), line, col)
     }
 
     #[inline]
     pub fn range(&self) -> Range<usize> {
         self.range.clone()
-    }
-
-    #[inline]
-    pub fn location(&self) -> (usize, usize) {
-        self.lexer.buffer.location_at(self.range.start)
     }
 
     #[inline]
