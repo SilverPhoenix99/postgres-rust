c40a9a817fbe881a3280d64bfb6ef02f3c641de4
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-09-10 20:20:52 +0100

    Added qn_fn_name.

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -1,3 +1,16 @@
+
+/// Qualified function name
+#[macro_export]
+macro_rules! qn_fn_name {
+    () => {{
+        let mut name = std::any::type_name_of_val(&||{});
+        while let Some(rest) = name.strip_suffix("::{{closure}}") {
+            name = rest
+        }
+        name
+    }};
+}
+
 mod concealable;
 pub mod guc;
 mod named;
