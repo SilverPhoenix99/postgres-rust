5e3aba605f008496b510131992188232c6ab1c3b
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-09-14 14:06:38 +0100

    Added ExtendedStringDecoder.

--- parser/src/string_decoders/mod.rs
+++ parser/src/string_decoders/mod.rs
@@ -1,10 +1,12 @@
 mod basic_decoder;
 mod bit_decoder;
 mod complex_decoder;
+mod extended_decoder;
 mod unicode_decoder;
 
 pub use basic_decoder::BasicStringDecoder;
 pub use bit_decoder::{BitStringDecoder, BitStringError};
+pub use extended_decoder::{ExtendedStringDecoder, ExtendedStringError};
 pub use unicode_decoder::{UnicodeStringDecoder, UnicodeStringError};
 
 #[inline(always)]
