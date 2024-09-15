11c6801926120c741bcdc315c59baab469187cef
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-09-15 20:51:53 +0100

    Added thiserror to LexerError. Added methods sqlstate() and detail().

--- parser/src/lexer/lexer_error.rs
+++ parser/src/lexer/lexer_error.rs
@@ -1,16 +1,66 @@
-#[derive(Debug, Copy, Clone, PartialEq)]
+use postgres_basics::sql_state::{ErrorSqlState, SqlState};
+
+#[derive(Debug, Copy, Clone, PartialEq, thiserror::Error)]
 pub enum LexerError {
-    UnknownChar { unknown: u8 },
+
+    #[error("Unexpected character {0:?}", *(.unknown) as char)]
+    UnexpectedChar { unknown: u8 },
+
+    #[error("unterminated /* comment")]
     UnterminatedBlockComment,
+
+    #[error("operator too long")]
     OperatorTooLong,
+
+    #[error("parameter number too large")]
     ParameterNumberTooLarge,
+
+    #[error("trailing junk after parameter")]
     TrailingJunkAfterParameter,
+
+    #[error("invalid {0} integer",
+        match .radix {
+            2 => "binary",
+            8 => "octal",
+            _ => "hexadecimal",
+        }
+    )]
     InvalidInteger { radix: i32 },
+
+    #[error("trailing junk after numeric literal")]
     TrailingJunkAfterNumericLiteral,
-    InvalidTokenRange,
-    UnterminatedString,
-    InvalidUnicodeEscape,
+
+    #[error("unterminated bit string literal")]
+    UnterminatedBitString,
+
+    #[error("unterminated hexadecimal string literal")]
+    UnterminatedHexString,
+
+    #[error("unterminated quoted string")]
+    UnterminatedQuotedString,
+
+    #[error("unterminated dollar-quoted string")]
+    UnterminatedDollarQuotedString,
+
+    #[error("zero-length delimited identifier")]
     EmptyDelimitedIdentifier,
-    UnsafeUnicodeString,
+
+    #[error("unterminated quoted identifier")]
     UnterminatedQuotedIdentifier,
+
+    #[error("unsafe use of string constant with Unicode escapes")]
+    UnsafeUnicodeString,
+}
+
+impl LexerError {
+    pub fn sqlstate(self) -> SqlState {
+        SqlState::Error(ErrorSqlState::SyntaxError)
+    }
+
+    pub fn detail(self) -> Option<&'static str> {
+        if self != Self::UnsafeUnicodeString {
+            return None
+        }
+        Some(r#"String constants with Unicode escapes cannot be used when "standard_conforming_strings" is off."#)
+    }
 }
