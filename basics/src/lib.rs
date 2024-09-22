771360edcb752e25e33b0537701ee83c3b9b3120
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-09-22 14:30:31 +0100

    Added getters to FnInfo. Added tests for qn_fn_name! and fn_info!.

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -11,9 +11,16 @@ macro_rules! qn_fn_name {
     }};
 }
 
+macro_rules! _fn_info {
+    () => { FnInfo::new(file!(), qn_fn_name!(), line!()) };
+}
+
 #[macro_export]
 macro_rules! fn_info {
-    () => { postgres_basics::FnInfo::new(file!(), postgres_basics::qn_fn_name!(), line!()) };
+    () => {{
+        use postgres_basics::{FnInfo, qn_fn_name};
+        _fn_info!()
+    }};
 }
 
 pub mod ascii;
@@ -37,3 +44,23 @@ pub use self::{
 pub type Oid = u32;
 
 pub const NAMEDATALEN: usize = 64;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_qn_fn_name() {
+        let fn_name = module_path!().to_owned() + "::test_qn_fn_name";
+        assert_eq!(fn_name, qn_fn_name!());
+    }
+
+    #[test]
+    fn test_fn_info() {
+        let fn_info = _fn_info!();
+
+        assert_eq!(file!(), fn_info.file_name());
+        assert_eq!(qn_fn_name!(), fn_info.function());
+        assert_eq!(line!() - 4, fn_info.line_number());
+    }
+}
