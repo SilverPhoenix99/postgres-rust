59e7c92102abd2914e233ea38c73a13e3adfebe6
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-09-22 14:30:31 +0100

    Added getters to FnInfo. Added tests for qn_fn_name! and fn_info!.

--- basics/src/fn_info.rs
+++ basics/src/fn_info.rs
@@ -1,5 +1,5 @@
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct FnInfo {
     file_name: &'static str,
     function: &'static str,
@@ -11,5 +11,20 @@ impl FnInfo {
     #[inline(always)]
     pub fn new(file_name: &'static str, function: &'static str, line_number: u32) -> Self {
         Self { file_name, function, line_number }
+    }
+
+    #[inline(always)]
+    pub fn file_name(&self) -> &'static str {
+        self.file_name
+    }
+
+    #[inline(always)]
+    pub fn function(&self) -> &'static str {
+        self.function
+    }
+
+    #[inline(always)]
+    pub fn line_number(&self) -> u32 {
+        self.line_number
     }
 }
