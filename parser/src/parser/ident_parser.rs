0df3c27467ad1e0d7183b9bcba0cf6b9650a1795
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-09-22 20:49:43 +0100

    Fixed bug in IdentifierParser. Added tests.

--- parser/src/parser/ident_parser.rs
+++ parser/src/parser/ident_parser.rs
@@ -45,6 +45,9 @@ impl<'p, 'src> IdentifierParser<'p, 'src> {
 
                 let escape = self.0.uescape()?;
 
+                // Strip delimiters:
+                let slice = &slice[3..slice.len() - 1];
+
                 UnicodeStringDecoder::new(slice, true, escape)
                     .decode()
                     .map_err(ParserError::UnicodeString)
@@ -64,5 +67,55 @@ impl<'p, 'src> IdentifierParser<'p, 'src> {
         }
 
         Ok(Some(ident))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parser::{ParseMode, Parser, ParserConfig};
+    use postgres_basics::guc::BackslashQuote;
+
+    #[test]
+    fn test_basic_ident() {
+        let mut parser = new_parser(b"some_identifier");
+        let mut ident_parser = IdentifierParser(&mut parser);
+
+        assert_eq!(Ok(Some("some_identifier".into())), ident_parser.parse());
+    }
+
+    #[test]
+    fn test_quoted_ident() {
+        let mut parser = new_parser(br#""quoted""#);
+        let mut ident_parser = IdentifierParser(&mut parser);
+
+        assert_eq!(Ok(Some("quoted".into())), ident_parser.parse());
+    }
+
+    #[test]
+    fn test_unicode_ident() {
+        let mut parser = new_parser(br#"u&"d\0061ta""#);
+        let mut ident_parser = IdentifierParser(&mut parser);
+
+        assert_eq!(Ok(Some("data".into())), ident_parser.parse());
+    }
+
+    #[test]
+    fn test_unicode_ident_with_uescape() {
+        let mut parser = new_parser(br#"u&"d!0061ta" UESCAPE '!'"#);
+        let mut ident_parser = IdentifierParser(&mut parser);
+
+        assert_eq!(Ok(Some("data".into())), ident_parser.parse());
+    }
+
+    fn new_parser(source: &[u8]) -> Parser<'_> {
+
+        let config = ParserConfig::new(
+            true,
+            BackslashQuote::SafeEncoding,
+            ParseMode::Default
+        );
+
+        Parser::new(source, config)
     }
 }
