9c3f913fd3189c9d96590e8f6ccc24a7960583d8
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-10-02 00:27:35 +0100

    Started implementation of HasSqlState and ErrorReport for ParserErrorKind.

--- parser/src/parser/error/parser_error_kind.rs
+++ parser/src/parser/error/parser_error_kind.rs
@@ -1,72 +1,171 @@
-#[derive(Debug, Default, Clone, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Eq, PartialEq, thiserror::Error)]
 pub enum ParserErrorKind {
     /// When a production fails.
     #[default]
+    #[error("syntax error")]
     Syntax,
 
     /// Still a syntax error, but thrown by the lexer
-    Lexer(LexerError),
+    #[error("{0}")]
+    Lexer(#[from] LexerError),
 
     /// When UESCAPE isn't followed by a simple 1 char string.
-    // "UESCAPE must be followed by a simple string literal"
+    #[error("UESCAPE must be followed by a simple string literal")]
     UescapeDelimiterMissing,
 
     /// When UESCAPE's delimiter string is invalid (len > 1, or invalid char).
-    // "invalid Unicode escape character"
+    #[error("invalid Unicode escape character")]
     InvalidUescapeDelimiter,
 
     /// When the input has an invalid UTF-8 char
+    #[error("TODO: invalid UTF-8")] // TODO
     Utf8(Utf8Error),
 
-    ExtendedString(ExtendedStringError),
+    #[error("{0}")]
+    ExtendedString(#[from] ExtendedStringError),
 
-    UnicodeString(UnicodeStringError),
+    #[error("{0}")]
+    UnicodeString(#[from] UnicodeStringError),
 
     /// When a bit string exceeds `VARBITMAXLEN`
+    #[error("TODO: bit string too long")] // TODO
     BitStringTooLong,
 
     /// When a char is not a valid binary digit in a bit string.
+    #[error("TODO: invalid binary digit")] // TODO
     InvalidBinaryDigit,
 
     /// When a Unicode escape (e.g., `'\XXXX'`) is invalid.
+    #[error("TODO: invalid Unicode codepoint")] // TODO
     InvalidUnicodeCodepoint,
 
     /// When a UTF-16 surrogate pair is invalid.
     /// E.g., the 2nd codepoint of the pair is invalid.
+    #[error("TODO: invalid Unicode surrogate pair")] // TODO
     InvalidUnicodeSurrogatePair,
 
     /// When using the '' escape in an unsafe string
+    #[error("TODO: non-standard use of escape character")] // TODO
     NonstandardUseOfEscapeCharacter,
 
     /// When "none" or "public" was incorrectly used as a role.
-    // #[error(r#"role name "{0}" is reserved"#)]
+    #[error(r#"role name "{0}" is reserved"#)]
     ReservedRoleSpec(&'static str),
 
     /// When a role is disallowed
-    // #[error(r#""#)]
+    #[error(r"{0} cannot be used as a role name here")]
     ForbiddenRoleSpec(&'static str),
 
     /// When the float precision is < 1
-    // #[error("precision for type float must be at least 1 bit")]
+    #[error("precision for type float must be at least 1 bit")]
     FloatPrecisionUnderflow(i32),
 
     /// When the float precision is > 53
-    // #[error("precision for type float must be less than 54 bits")]
+    #[error("precision for type float must be less than 54 bits")]
     FloatPrecisionOverflow(i32),
 
     /// When an identifier is used as an unrecognized role option
-    // #[error(r#"unrecognized role option "{0}""#)]
+    #[error(r#"unrecognized role option "{0}""#)]
     UnrecognizedRoleOption(String),
 
     /// When "UNENCRYPTED PASSWORD" is used as a role option
-    // #[error("UNENCRYPTED PASSWORD is no longer supported")]
-    // hint = "Remove UNENCRYPTED to store the password in encrypted form instead."
+    #[error("UNENCRYPTED PASSWORD is no longer supported")]
     UnencryptedPassword,
 }
 
-impl From<LexerError> for ParserErrorKind {
-    fn from(value: LexerError) -> Self {
-        Lexer(value)
+impl HasSqlState for ParserErrorKind {
+    fn sql_state(&self) -> SqlState {
+        match self {
+            ExtendedString(err) => err.sql_state(),
+            FloatPrecisionOverflow(_) => SqlState::Error(InvalidParameterValue),
+            FloatPrecisionUnderflow(_) => SqlState::Error(InvalidParameterValue),
+            ForbiddenRoleSpec(_) => SqlState::Error(ReservedName),
+            InvalidUescapeDelimiter => SqlState::Error(SyntaxError),
+            InvalidUnicodeCodepoint => {todo!()},
+            InvalidUnicodeSurrogatePair => {todo!()},
+            Lexer(err) => err.sql_state(),
+            NonstandardUseOfEscapeCharacter => {todo!()},
+            ReservedRoleSpec(_) => SqlState::Error(ReservedName),
+            Self::BitStringTooLong => {todo!()},
+            Self::InvalidBinaryDigit => {todo!()},
+            Syntax => SqlState::Error(SyntaxError),
+            UescapeDelimiterMissing => SqlState::Error(SyntaxError),
+            UnencryptedPassword => SqlState::Error(FeatureNotSupported),
+            UnicodeString(err) => err.sql_state(),
+            UnrecognizedRoleOption(_) => SqlState::Error(SyntaxError),
+            Utf8(_) => {todo!()},
+        }
+    }
+}
+
+impl ErrorReport for ParserErrorKind {
+    fn hint(&self) -> Option<Cow<'static, str>> {
+        match self {
+            ExtendedString(err) => err.hint(),
+            FloatPrecisionOverflow(_) => None,
+            FloatPrecisionUnderflow(_) => None,
+            ForbiddenRoleSpec(_) => None,
+            InvalidUescapeDelimiter => None,
+            InvalidUnicodeCodepoint => {todo!()},
+            InvalidUnicodeSurrogatePair => {todo!()},
+            Lexer(err) => err.hint(),
+            NonstandardUseOfEscapeCharacter => {todo!()},
+            ReservedRoleSpec(_) => None,
+            Self::BitStringTooLong => {todo!()},
+            Self::InvalidBinaryDigit => {todo!()},
+            Syntax => None,
+            UescapeDelimiterMissing => None,
+            UnencryptedPassword => Some("Remove UNENCRYPTED to store the password in encrypted form instead.".into()),
+            UnicodeString(err) => err.hint(),
+            UnrecognizedRoleOption(_) => None,
+            Utf8(_) => {todo!()},
+        }
+    }
+
+    fn detail(&self) -> Option<Cow<'static, str>> {
+        match self {
+            ExtendedString(err) => err.detail(),
+            FloatPrecisionOverflow(_) => None,
+            FloatPrecisionUnderflow(_) => None,
+            ForbiddenRoleSpec(_) => None,
+            InvalidUescapeDelimiter => None,
+            InvalidUnicodeCodepoint => {todo!()},
+            InvalidUnicodeSurrogatePair => {todo!()},
+            Lexer(err) => err.detail(),
+            NonstandardUseOfEscapeCharacter => {todo!()},
+            ReservedRoleSpec(_) => None,
+            Self::BitStringTooLong => {todo!()},
+            Self::InvalidBinaryDigit => {todo!()},
+            Syntax => None,
+            UescapeDelimiterMissing => None,
+            UnencryptedPassword => None,
+            UnicodeString(err) => err.detail(),
+            UnrecognizedRoleOption(_) => None,
+            Utf8(_) => {todo!()},
+        }
+    }
+
+    fn detail_log(&self) -> Option<Cow<'static, str>> {
+        match self {
+            ExtendedString(err) => err.detail_log(),
+            FloatPrecisionOverflow(_) => None,
+            FloatPrecisionUnderflow(_) => None,
+            ForbiddenRoleSpec(_) => None,
+            InvalidUescapeDelimiter => None,
+            InvalidUnicodeCodepoint => {todo!()},
+            InvalidUnicodeSurrogatePair => {todo!()},
+            Lexer(err) => err.detail_log(),
+            NonstandardUseOfEscapeCharacter => {todo!()},
+            ReservedRoleSpec(_) => None,
+            Self::BitStringTooLong => {todo!()},
+            Self::InvalidBinaryDigit => {todo!()},
+            Syntax => None,
+            UescapeDelimiterMissing => None,
+            UnencryptedPassword => None,
+            UnicodeString(err) => err.detail_log(),
+            UnrecognizedRoleOption(_) => None,
+            Utf8(_) => {todo!()},
+        }
     }
 }
 
@@ -85,14 +184,23 @@ impl From<FromUtf8Error> for ParserErrorKind {
 impl From<BitStringError> for ParserErrorKind {
     fn from(value: BitStringError) -> Self {
         match value {
-            BitStringError::BitStringTooLong => BitStringTooLong,
-            BitStringError::InvalidBinaryDigit => InvalidBinaryDigit,
+            BitStringTooLong => Self::BitStringTooLong,
+            InvalidBinaryDigit => Self::InvalidBinaryDigit,
         }
     }
 }
 
 use crate::lexer::LexerError;
-use crate::string_decoders::{BitStringError, ExtendedStringError, UnicodeStringError};
+use crate::string_decoders::{
+    BitStringError,
+    BitStringError::{BitStringTooLong, InvalidBinaryDigit},
+    ExtendedStringError,
+    UnicodeStringError
+};
+use postgres_basics::elog::{ErrorReport, HasSqlState};
+use postgres_basics::sql_state::ErrorSqlState::{FeatureNotSupported, InvalidParameterValue, ReservedName, SyntaxError};
+use postgres_basics::sql_state::SqlState;
+use std::borrow::Cow;
 use std::str::Utf8Error;
 use std::string::FromUtf8Error;
 use ParserErrorKind::*;
