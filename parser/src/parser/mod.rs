1c48d0e4257616d90a5ed05e6cdad5d4e8446994
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-10-18 20:06:10 +0100

    Moved Parser.stmt to its own module.

--- parser/src/parser/mod.rs
+++ parser/src/parser/mod.rs
@@ -1,17 +1,18 @@
 pub mod ast_node;
 mod bit_string_parser;
 mod config;
+mod consume_macro;
 mod error;
 mod expr_parsers;
 mod ident_parser;
 mod op_parsers;
 mod result;
 mod role_parsers;
+mod stmt_parser;
 mod stmt_parsers;
 mod string_parser;
 mod token_buffer;
 mod warning;
-mod consume_macro;
 
 pub use self::{
     config::ParserConfig,
@@ -124,52 +125,6 @@ impl<'src> Parser<'src> {
 
     fn toplevel_stmt(&mut self) -> ParseResult<RawStmt> {
         self.stmt(true)
-    }
-
-    fn stmt(&mut self, allow_tx_legacy_stmts: bool) -> ParseResult<RawStmt> {
-        use TokenKind::Keyword as Kw;
-        use Keyword::*;
-
-        consume!{self default,
-            Kw(Begin) if allow_tx_legacy_stmts => self.begin_stmt().map(From::from),
-            Kw(End) if allow_tx_legacy_stmts => self.end_stmt().map(From::from),
-            Kw(Checkpoint) => Ok(RawStmt::CheckPoint),
-            Kw(Abort) => self.abort_stmt().map(From::from),
-            Kw(Alter) => self.alter_stmt(),
-            Kw(Analyse | Analyze) => self.analyze_stmt(),
-            Kw(Call) => self.call_stmt(),
-            Kw(Close) => self.close_stmt().map(ClosePortalStmt),
-            Kw(Cluster) => self.cluster_stmt(),
-            Kw(Comment) => self.comment_stmt(),
-            Kw(Commit) => self.commit_stmt().map(From::from),
-            Kw(CopyKw) => self.copy_stmt(),
-            Kw(Deallocate) => self.deallocate_stmt().map(DeallocateStmt),
-            Kw(Discard) => self.discard_stmt().map(From::from),
-            Kw(Do) => self.do_stmt(),
-            Kw(DropKw) => self.drop_stmt(),
-            Kw(Explain) => self.explain_stmt(),
-            Kw(Fetch) => self.fetch_stmt(),
-            Kw(Import) => self.import_stmt(),
-            Kw(Listen) => self.listen_stmt().map(ListenStmt),
-            Kw(Load) => self.load_stmt().map(LoadStmt),
-            Kw(Lock) => self.lock_stmt(),
-            Kw(Move) => self.move_stmt(),
-            Kw(Notify) => self.notify_stmt().map(From::from),
-            Kw(Prepare) => self.prepare_stmt(),
-            Kw(Reassign) => self.reassign_owned_stmt().map(From::from),
-            Kw(Reindex) => self.reindex_stmt(),
-            Kw(Release) => self.release_savepoint_stmt().map(From::from),
-            Kw(Revoke) => self.revoke_stmt(),
-            Kw(Rollback) => self.rollback_stmt().map(From::from),
-            Kw(Savepoint) => self.savepoint_stmt().map(From::from),
-            Kw(Security) => self.security_stmt(),
-            Kw(Set) => self.set_stmt(),
-            Kw(Show) => self.show_stmt().map(From::from),
-            Kw(Start) => self.start_transaction_stmt().map(From::from),
-            Kw(Truncate) => self.truncate_stmt(),
-            Kw(Unlisten) => self.unlisten_stmt().map(UnlistenStmt),
-            Kw(Vacuum) => self.vacuum_stmt(),
-        }
     }
 
     fn opt_transaction(&mut self) -> ParseResult<()> {
@@ -860,42 +815,6 @@ mod tests {
     }
 
     #[test]
-    fn test_stmt() {
-        let sources = [
-            // TODO: analyze, call, cluster, comment, copy, do, drop, explain, fetch, import, lock, move,
-            //       reindex, revoke, security, set, truncate, vacuum
-            "abort transaction",
-            "alter group some_group add user public",
-            "close all",
-            "commit and no chain",
-            "deallocate all",
-            "discard all",
-            "listen ident",
-            "load 'test string'",
-            "notify test_ident, 'test-payload'",
-            "prepare transaction 'tx id'",
-            "reassign owned by public, test_role to target_role",
-            "release savepoint test_ident",
-            "rollback to test_ident",
-            "savepoint test_ident",
-            "show all",
-            "start transaction read only, read write deferrable",
-            "unlisten *",
-        ];
-
-        for source in sources {
-            let mut parser = Parser::new(source, DEFAULT_CONFIG);
-            let actual = parser.stmt(true);
-
-            // This only quickly tests that statement types aren't missing.
-            // More in-depth testing is within each statement's module.
-            assert_matches!(actual, Ok(_),
-                r"expected Ok(Some(_)) for {source:?} but actually got {actual:?}"
-            )
-        }
-    }
-
-    #[test]
     fn test_opt_transaction() {
         let mut parser = Parser::new("transaction work", DEFAULT_CONFIG);
         assert_eq!(Ok(()), parser.opt_transaction());
@@ -1240,7 +1159,7 @@ use self::{
         EventTriggerState,
         ExprNode,
         IsolationLevel,
-        RawStmt::{self, ClosePortalStmt, DeallocateStmt, ListenStmt, LoadStmt, UnlistenStmt},
+        RawStmt::{self},
         RoleSpec,
         SignedNumber,
         SystemType::{self, Bool, Float4, Float8, Int2, Int4, Int8},
