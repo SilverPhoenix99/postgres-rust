16b45ae6c46aca229b026ac290156b5b05aeeee3
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-10-28 19:04:14 +0000

    Refactor operators.

--- parser/src/parser/op_parsers.rs
+++ parser/src/parser/op_parsers.rs
@@ -1,33 +1,87 @@
+bitflags! {
+  pub(in super) struct OperatorKind : u8 {
+    const Additive       = 1;
+    const Multiplicative = 1 << 1;
+    const Exponentiation = 1 << 2;
+    const Boolean        = 1 << 3;
+    const Explicit       = 1 << 4;
+    const UserDefined    = 1 << 5;
+
+    const Math = Self::Additive.bits()
+               | Self::Multiplicative.bits()
+               | Self::Exponentiation.bits()
+               | Self::Boolean.bits();
+
+    const Qualified = Self::UserDefined.bits() | Self::Explicit.bits();
+    const Unqualified = Self::UserDefined.bits() | Self::Math.bits();
+    const All = Self::Unqualified.bits() | Self::Qualified.bits();
+  }
+}
+
 impl<'src> Parser<'src> {
 
     /// Alias: `qual_Op`
     pub(super) fn qual_op(&mut self) -> ScanResult<QnOperator> {
-
-        /*
-            Operator | prefixed_operator
-        */
-
-        if let Some(op) = self.operator().no_match_to_option()? {
-            let op = AllOp::Operator(op);
-            return Ok(QnOperator(vec![], op))
-        }
-
-        self.prefixed_operator()
+        self.operator(OperatorKind::Qualified)
     }
 
-    /// Production: `OPERATOR '(' any_operator ')'`
-    pub(super) fn prefixed_operator(&mut self) -> ScanResult<QnOperator> {
-        use crate::lexer::Keyword::Operator;
-        use crate::lexer::TokenKind::{CloseParenthesis, OpenParenthesis};
-        const FN_NAME: &str = "postgres_parser::parser::Parser::prefixed_operator";
-
-        self.buffer.consume_kw_eq(Operator)?;
-
-        self.buffer.consume_eq(OpenParenthesis).required(fn_info!(FN_NAME))?;
-        let op = self.any_operator().required(fn_info!(FN_NAME))?;
-        self.buffer.consume_eq(CloseParenthesis).required(fn_info!(FN_NAME))?;
-
+    /// Alias: `all_Op`
+    pub(super) fn all_op(&mut self) -> ScanResult<Operator> {
+        let QnOperator(_qn, op) = self.operator(OperatorKind::Unqualified)?;
+        debug_assert!(_qn.is_empty());
         Ok(op)
+    }
+
+    /// Alias: `qual_all_Op`
+    pub(super) fn qual_all_op(&mut self) -> ScanResult<QnOperator> {
+        self.operator(OperatorKind::All)
+    }
+
+    pub(super) fn operator(&mut self, kind: OperatorKind) -> ScanResult<QnOperator> {
+        use TokenKind::{Equals, Greater, GreaterEquals, Keyword as Kw, Less, LessEquals, NotEquals, };
+        use crate::lexer::Keyword::Operator as OperatorKw;
+
+        const FN_NAME: &str = "postgres_parser::parser::Parser::operator";
+
+        let loc = self.buffer.current_location();
+
+        consume! {self
+            ok {
+                Ok(Plus) if kind.intersects(OperatorKind::Additive) => Ok(Addition.into()),
+                Ok(Minus) if kind.intersects(OperatorKind::Additive) => Ok(Subtraction.into()),
+                Ok(Mul) if kind.intersects(OperatorKind::Multiplicative) => Ok(Multiplication.into()),
+                Ok(Div) if kind.intersects(OperatorKind::Multiplicative) => Ok(Division.into()),
+                Ok(Percent) if kind.intersects(OperatorKind::Multiplicative) => Ok(Modulo.into()),
+                Ok(Circumflex) if kind.intersects(OperatorKind::Exponentiation) => Ok(Exponentiation.into()),
+                Ok(Less) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::Less.into()),
+                Ok(Equals) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::Equals.into()),
+                Ok(Greater) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::Greater.into()),
+                Ok(LessEquals) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::LessEquals.into()),
+                Ok(GreaterEquals) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::GreaterEquals.into()),
+                Ok(NotEquals) if kind.intersects(OperatorKind::Boolean) => Ok(Operator::NotEquals.into()),
+                Ok(UserDefinedOperator) if kind.intersects(OperatorKind::UserDefined) => {
+                    let source = self.buffer.source();
+                    let op = loc.slice(source).to_owned();
+                    Ok(UserDefined(op).into())
+                },
+                Ok(Kw(OperatorKw)) if kind.intersects(OperatorKind::Explicit) => {
+
+                    /*
+                        `OPERATOR '(' any_operator ')'`
+                    */
+
+                    self.buffer.consume_eq(OpenParenthesis).required(fn_info!(FN_NAME))?;
+                    let op = self.any_operator().required(fn_info!(FN_NAME))?;
+                    self.buffer.consume_eq(CloseParenthesis).required(fn_info!(FN_NAME))?;
+
+                    Ok(op)
+                },
+            }
+            err {
+                Ok(_) => Err(NoMatch),
+                Err(err) => Err(err.into()),
+            }
+        }
     }
 
     pub(super) fn any_operator(&mut self) -> ScanResult<QnOperator> {
@@ -56,52 +110,6 @@ impl<'src> Parser<'src> {
         let op = QnOperator(qn, op);
         Ok(op)
     }
-
-    /// Alias: `all_Op`
-    pub(super) fn all_op(&mut self) -> ScanResult<AllOp> {
-
-        if let Some(op) = self.math_op().no_match_to_option()? {
-            return Ok(AllOp::MathOp(op))
-        }
-
-        self.operator().map(AllOp::Operator)
-    }
-
-    /// Returns the text of the `UserDefinedOperator`
-    pub(super) fn operator(&mut self) -> ScanResult<String> {
-
-        let loc = self.buffer.current_location();
-        let source = self.buffer.source();
-        self.buffer.consume(|tok| match tok {
-            TokenKind::UserDefinedOperator => {
-                let op = loc.slice(source).to_owned();
-                Some(op)
-            },
-            _ => None
-        })
-    }
-
-    /// Alias: `MathOp`
-    pub(super) fn math_op(&mut self) -> ScanResult<MathOp> {
-        use crate::parser::ast_node::MathOp::*;
-
-        self.buffer.consume(|tok| match tok {
-            TokenKind::Plus => Some(Addition),
-            TokenKind::Minus => Some(Subtraction),
-            TokenKind::Mul => Some(Multiplication),
-            TokenKind::Div => Some(Division),
-            TokenKind::Percent => Some(Modulo),
-            TokenKind::Circumflex => Some(Exponentiation),
-            TokenKind::Less => Some(Less),
-            TokenKind::Greater => Some(Greater),
-            TokenKind::Equals => Some(Equals),
-            TokenKind::LessEquals => Some(LessEquals),
-            TokenKind::GreaterEquals => Some(GreaterEquals),
-            TokenKind::NotEquals => Some(NotEquals),
-            _ => None
-        })
-    }
-
 }
 
 #[cfg(test)]
@@ -109,118 +117,107 @@ mod tests {
     use super::*;
     use crate::parser::tests::DEFAULT_CONFIG;
 
-
     #[test]
     fn test_qual_op() {
-        use crate::parser::ast_node::{AllOp, QnOperator};
 
         let source = "operator(|/) <@>";
         let mut parser = Parser::new(source, DEFAULT_CONFIG);
 
-        let expected = QnOperator(
-            vec![],
-            AllOp::Operator("|/".into())
-        );
+        let expected = QnOperator(vec![], UserDefined("|/".into()));
         assert_eq!(Ok(expected), parser.qual_op());
 
-        let expected = QnOperator(
-            vec![],
-            AllOp::Operator("<@>".into())
-        );
+        let expected = QnOperator(vec![], UserDefined("<@>".into()));
         assert_eq!(Ok(expected), parser.qual_op());
     }
 
     #[test]
-    fn test_prefixed_operator() {
-        use crate::parser::ast_node::{AllOp, MathOp, QnOperator};
-
+    fn test_qualified_operator() {
         let source = "operator(some_qn.*)";
         let mut parser = Parser::new(source, DEFAULT_CONFIG);
 
-        let actual = parser.prefixed_operator();
+        let actual = parser.qual_op();
         let expected = QnOperator(
             vec!["some_qn".into()],
-            AllOp::MathOp(MathOp::Multiplication)
+            Multiplication
         );
         assert_eq!(Ok(expected), actual);
     }
 
     #[test]
     fn test_any_operator() {
-        use crate::parser::ast_node::{AllOp, MathOp, QnOperator};
-
         let source = "@@ != qn_name.+";
         let mut parser = Parser::new(source, DEFAULT_CONFIG);
 
         let expected = QnOperator(
             vec![],
-            AllOp::Operator("@@".into())
+            UserDefined("@@".into())
         );
         assert_eq!(Ok(expected), parser.any_operator());
 
         let expected = QnOperator(
             vec![],
-            AllOp::MathOp(MathOp::NotEquals)
+            NotEquals
         );
         assert_eq!(Ok(expected), parser.any_operator());
 
         let expected = QnOperator(
             vec!["qn_name".into()],
-            AllOp::MathOp(MathOp::Addition)
+            Addition
         );
         assert_eq!(Ok(expected), parser.any_operator());
     }
 
     #[test]
     fn test_all_op() {
-        use AllOp::*;
-        use crate::parser::ast_node::MathOp::NotEquals;
-
         let source = "~@ <>";
         let mut parser = Parser::new(source, DEFAULT_CONFIG);
 
-        assert_eq!(Ok(Operator("~@".into())), parser.all_op());
-        assert_eq!(Ok(MathOp(NotEquals)), parser.all_op());
-    }
-
-    #[test]
-    fn test_operator() {
-        let source = "~@";
-        let mut parser = Parser::new(source, DEFAULT_CONFIG);
-
-        assert_eq!(Ok("~@".into()), parser.operator());
+        assert_eq!(Ok(UserDefined("~@".into())), parser.all_op());
+        assert_eq!(Ok(NotEquals), parser.all_op());
     }
 
     #[test]
     fn test_math_op() {
-        use MathOp::*;
 
         let source = "+ - * / % ^ < > = <= >= != <>";
         let mut parser = Parser::new(source, DEFAULT_CONFIG);
 
-        assert_eq!(Ok(Addition), parser.math_op());
-        assert_eq!(Ok(Subtraction), parser.math_op());
-        assert_eq!(Ok(Multiplication), parser.math_op());
-        assert_eq!(Ok(Division), parser.math_op());
-        assert_eq!(Ok(Modulo), parser.math_op());
-        assert_eq!(Ok(Exponentiation), parser.math_op());
-        assert_eq!(Ok(Less), parser.math_op());
-        assert_eq!(Ok(Greater), parser.math_op());
-        assert_eq!(Ok(Equals), parser.math_op());
-        assert_eq!(Ok(LessEquals), parser.math_op());
-        assert_eq!(Ok(GreaterEquals), parser.math_op());
-        assert_eq!(Ok(NotEquals), parser.math_op());
-        assert_eq!(Ok(NotEquals), parser.math_op());
+        assert_eq!(Ok(Addition), parser.all_op());
+        assert_eq!(Ok(Subtraction), parser.all_op());
+        assert_eq!(Ok(Multiplication), parser.all_op());
+        assert_eq!(Ok(Division), parser.all_op());
+        assert_eq!(Ok(Modulo), parser.all_op());
+        assert_eq!(Ok(Exponentiation), parser.all_op());
+        assert_eq!(Ok(Less), parser.all_op());
+        assert_eq!(Ok(Greater), parser.all_op());
+        assert_eq!(Ok(Equals), parser.all_op());
+        assert_eq!(Ok(LessEquals), parser.all_op());
+        assert_eq!(Ok(GreaterEquals), parser.all_op());
+        assert_eq!(Ok(NotEquals), parser.all_op());
+        assert_eq!(Ok(NotEquals), parser.all_op());
     }
 }
 
 use crate::{
-    lexer::TokenKind::{self, Dot},
+    lexer::TokenKind::{
+        self,
+        Circumflex,
+        CloseParenthesis,
+        Div,
+        Dot,
+        Minus,
+        Mul,
+        OpenParenthesis,
+        Percent,
+        Plus,
+        UserDefinedOperator,
+    },
     parser::{
-        ast_node::{AllOp, MathOp, QnOperator},
-        result::{Optional, Required, ScanResult, ScanResultTrait},
-        token_buffer::TokenConsumer,
+        ast_node::{Operator::{self, *}, QnOperator},
+        consume_macro::consume,
+        result::{Optional, Required, ScanErrorKind::NoMatch, ScanResult},
         Parser
     }
 };
+use bitflags::bitflags;
 use postgres_basics::fn_info;
