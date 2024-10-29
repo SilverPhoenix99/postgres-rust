879eca72ddd4b31d077c480c8311f76f215a946c
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2024-10-29 18:36:58 +0000

    Added def_acl_action parser.

--- parser/src/parser/acl_parsers/mod.rs
+++ parser/src/parser/acl_parsers/mod.rs
@@ -3,6 +3,59 @@ mod privilege_parsers;
 
 impl Parser<'_> {
 
+    /// Alias `DefACLAction`
+    pub(in crate::parser) fn def_acl_action(&mut self) -> ScanResult<GrantStmt> {
+        const FN_NAME: &str = "postgres_parser::parser::Parser::def_acl_action";
+
+        /*
+              GRANT privileges ON defacl_privilege_target TO grantee_list opt_grant_option
+            | REVOKE ( GRANT OPTION FOR )? privileges ON defacl_privilege_target FROM grantee_list opt_drop_behavior
+        */
+
+        consume!{self
+            Ok {
+                Kw(Grant) => {
+                    let privileges = self.privileges().required(fn_info!(FN_NAME))?;
+                    self.buffer.consume_kw_eq(On).required(fn_info!(FN_NAME))?;
+                    let object_type = self.def_acl_privilege_target().required(fn_info!(FN_NAME))?;
+                    self.buffer.consume_kw_eq(To).required(fn_info!(FN_NAME))?;
+                    let grantees = self.grantee_list().required(fn_info!(FN_NAME))?;
+                    let grant_option = self.opt_grant_option().required(fn_info!(FN_NAME))?;
+
+                    let stmt = GrantStmt::grant(privileges, object_type, grantees, grant_option);
+                    Ok(stmt)
+                },
+                Kw(Revoke) => {
+                    let grant_option = if self.buffer.consume_kw_eq(Grant).optional()?.is_some() {
+                        /*
+                            GRANT OPTION FOR
+                        */
+                        self.buffer.consume_kw_eq(OptionKw).required(fn_info!(FN_NAME))?;
+                        self.buffer.consume_kw_eq(For).required(fn_info!(FN_NAME))?;
+                        true
+                    }
+                    else {
+                        false
+                    };
+
+                    let privileges = self.privileges().required(fn_info!(FN_NAME))?;
+                    self.buffer.consume_kw_eq(On).required(fn_info!(FN_NAME))?;
+                    let object_type = self.def_acl_privilege_target().required(fn_info!(FN_NAME))?;
+                    self.buffer.consume_kw_eq(FromKw).required(fn_info!(FN_NAME))?;
+                    let grantees = self.grantee_list().required(fn_info!(FN_NAME))?;
+                    let drop_behavior = self.opt_drop_behavior().required(fn_info!(FN_NAME))?;
+
+                    let stmt = GrantStmt::revoke(privileges, object_type, grantees, grant_option, drop_behavior);
+                    Ok(stmt)
+                },
+            }
+            Err {
+                Ok(_) => NoMatch,
+                Err(err) => err.into(),
+            }
+        }
+    }
+
     /// Post-condition: Vec is **Not** empty
     pub(in crate::parser) fn grantee_list(&mut self) -> ScanResult<Vec<RoleSpec>> {
         const FN_NAME: &str = "postgres_parser::parser::Parser::grantee_list";
@@ -109,7 +162,7 @@ impl Parser<'_> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::parser::ast_node::RoleSpec;
+    use crate::parser::ast_node::{AccessPrivilege, RoleSpec};
     use crate::parser::tests::DEFAULT_CONFIG;
     use test_case::test_case;
 
@@ -165,6 +218,76 @@ mod tests {
         let mut parser = Parser::new("granted by public", DEFAULT_CONFIG);
         assert_eq!(Ok(RoleSpec::Public), parser.opt_granted_by());
     }
+
+    #[test]
+    fn test_grant_def_acl_action() {
+        let source = "grant all privileges on tables to public";
+        let mut parser = Parser::new(source, DEFAULT_CONFIG);
+
+        let actual = parser.def_acl_action();
+
+        let expected = GrantStmt::grant(
+            AccessPrivilege::All(None),
+            AclTarget::Table,
+            vec![RoleSpec::Public],
+            false
+        );
+
+        assert_eq!(Ok(expected), actual);
+    }
+
+    #[test]
+    fn test_grant_with_option_def_acl_action() {
+        let source = "grant all privileges on tables to public with grant option";
+        let mut parser = Parser::new(source, DEFAULT_CONFIG);
+
+        let actual = parser.def_acl_action();
+
+        let expected = GrantStmt::grant(
+            AccessPrivilege::All(None),
+            AclTarget::Table,
+            vec![RoleSpec::Public],
+            true
+        );
+
+        assert_eq!(Ok(expected), actual);
+    }
+
+    #[test]
+    fn test_revoke_def_acl_action() {
+        let source = "revoke all privileges on tables from public";
+        let mut parser = Parser::new(source, DEFAULT_CONFIG);
+
+        let actual = parser.def_acl_action();
+
+        let expected = GrantStmt::revoke(
+            AccessPrivilege::All(None),
+            AclTarget::Table,
+            vec![RoleSpec::Public],
+            false,
+            DropBehavior::Restrict
+        );
+
+        assert_eq!(Ok(expected), actual);
+    }
+
+    #[test]
+    fn test_revoke_grant_option_cascade_def_acl_action() {
+        let source = "revoke grant option for all privileges on tables from public cascade";
+        let mut parser = Parser::new(source, DEFAULT_CONFIG);
+
+        let actual = parser.def_acl_action();
+
+        let expected = GrantStmt::revoke(
+            AccessPrivilege::All(None),
+            AclTarget::Table,
+            vec![RoleSpec::Public],
+            true,
+            DropBehavior::Cascade
+        );
+
+        assert_eq!(Ok(expected), actual);
+    }
 }
 
 use crate::{
@@ -172,16 +295,21 @@ use crate::{
         Keyword::{
             By,
             Cascade,
+            For,
+            FromKw,
             Functions,
             Grant,
             Granted,
             Group,
+            On,
             OptionKw,
             Restrict,
+            Revoke,
             Routines,
             Schemas,
             Sequences,
             Tables,
+            To,
             Types,
             With,
         },
@@ -191,7 +319,7 @@ use crate::{
         }
     },
     parser::{
-        ast_node::{AclTarget, DropBehavior, RoleSpec},
+        ast_node::{AclTarget, DropBehavior, GrantStmt, RoleSpec},
         consume_macro::consume,
         result::{EofResult, Optional, Required, ScanErrorKind::NoMatch, ScanResult, ScanResultTrait},
         Parser
