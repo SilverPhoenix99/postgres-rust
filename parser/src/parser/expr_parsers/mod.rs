e53189dbc635138b91b79dce112eb58250becf4f
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-11-26 19:13:30 +0000

    Removed the remaining old parsers.

--- parser/src/parser/expr_parsers/mod.rs
+++ parser/src/parser/expr_parsers/mod.rs
@@ -1,25 +1,10 @@
-mod indirection;
 mod associativity;
-mod a_expr_prec;
-mod b_expr_prec;
 mod case_expr;
 mod expr_const;
 mod expr_primary;
+mod indirection;
 
 pub(super) use indirection::indirection;
-
-impl Parser<'_> {
-
-    #[inline(always)]
-    pub(in crate::parser) fn a_expr(&mut self) -> ScanResult<ExprNode> {
-        self.a_expr_prec(0)
-    }
-
-    #[inline(always)]
-    pub(in crate::parser) fn b_expr(&mut self) -> ScanResult<ExprNode> {
-        self.b_expr_prec(0)
-    }
-}
 
 pub(in crate::parser) fn a_expr() -> impl Combinator<Output = ExprNode> {
     // TODO
@@ -33,6 +18,4 @@ pub(in crate::parser) fn b_expr() -> impl Combinator<Output = ExprNode> {
 
 use crate::parser::ast_node::ExprNode;
 use crate::parser::combinators::Combinator;
-use crate::parser::result::ScanResult;
-use crate::parser::Parser;
 use expr_primary::expr_primary;
