3188019702f295047378b8769af3358fa4811c64
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-12-05 17:48:39 +0000

    Replaced keyword_if with an or() combinator.

--- parser/src/parser/opt_transaction.rs
+++ parser/src/parser/opt_transaction.rs
@@ -1,10 +1,8 @@
 pub(super) fn opt_transaction() -> impl Combinator<Output = ()> {
-    use crate::lexer::Keyword::{Transaction, Work};
-    use crate::parser::combinators::{keyword_if, CombinatorHelpers};
 
     // Skips over WORK | TRANSACTION
 
-    keyword_if(|kw| matches!(kw, Work | Transaction))
+    Work.or(Transaction)
         .optional()
         .skip()
 }
@@ -24,4 +22,7 @@ mod tests {
     }
 }
 
+use crate::lexer::Keyword::Transaction;
+use crate::lexer::Keyword::Work;
 use crate::parser::combinators::Combinator;
+use crate::parser::combinators::CombinatorHelpers;
