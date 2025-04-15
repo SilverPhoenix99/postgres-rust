824fd327a287b385c277cf20000dafa6c8ab542b
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2025-04-15 21:31:48 +0100

    Implemented CREATE USER statement.

---
+++ parser/src/parser/ast_node/create_user_mapping_stmt.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CreateUserMappingStmt {
+    user: RoleSpec,
+    server: Str,
+    options: Vec<GenericOption>,
+    if_not_exists: bool
+}
+
+impl CreateUserMappingStmt {
+    pub fn new<T: Into<Str>>(user: RoleSpec, server: T, options: Vec<GenericOption>, if_not_exists: bool) -> Self {
+        Self {
+            user,
+            server: server.into(),
+            options,
+            if_not_exists
+        }
+    }
+
+    pub fn user(&self) -> &RoleSpec {
+        &self.user
+    }
+
+    pub fn server(&self) -> &str {
+        &self.server
+    }
+
+    pub fn options(&self) -> &[GenericOption] {
+        &self.options
+    }
+
+    pub fn if_not_exists(&self) -> bool {
+        self.if_not_exists
+    }
+}
+
+use crate::parser::ast_node::GenericOption;
+use crate::parser::ast_node::RoleSpec;
+use postgres_basics::Str;
