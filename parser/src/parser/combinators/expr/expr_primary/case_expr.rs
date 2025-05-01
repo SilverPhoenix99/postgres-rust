7e827587afa625d29f026e1779c76a1a3e1e9be6
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2025-05-01 18:22:11 +0100

    Added tests for case_expr.

--- parser/src/parser/combinators/expr/expr_primary/case_expr.rs
+++ parser/src/parser/combinators/expr/expr_primary/case_expr.rs
@@ -10,7 +10,7 @@ pub(super) fn case_expr() -> impl Combinator<Output = CaseExpr> {
     sequence!(
         Case.skip(),
         a_expr().optional(),
-        many(when_clause()).required(),
+        many(when_clause()),
         else_clause()
     ).map(|(_, target, when_clauses, default)|
         CaseExpr::new(target, when_clauses, default)
@@ -18,12 +18,10 @@ pub(super) fn case_expr() -> impl Combinator<Output = CaseExpr> {
 }
 
 fn when_clause() -> impl Combinator<Output = CaseWhen> {
-    sequence!(
-        When.skip(),
-        a_expr(),
-        Then.skip(),
-        a_expr()
-    ).map(|(_, condition, _, body)|
+    and(
+        When.and_right(a_expr()),
+        Then.and_right(a_expr()),
+    ).map(|(condition, body)|
         CaseWhen::new(condition, body)
     )
 }
@@ -36,9 +34,52 @@ fn else_clause() -> impl Combinator<Output = Option<ExprNode>> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::parser::tests::test_parser;
+
+    #[test]
+    fn test_case_value_when_else() {
+        test_parser!(
+            source = "
+                CASE 'foo'
+                    WHEN 1 THEN 2
+                    ELSE 3
+                END
+            ",
+            parser = case_expr(),
+            expected = CaseExpr::new(
+                Some(ExprNode::StringConst("foo".into())),
+                vec![
+                    CaseWhen::new(
+                        ExprNode::IntegerConst(1),
+                        ExprNode::IntegerConst(2),
+                    ),
+                ],
+                Some(ExprNode::IntegerConst(3)),
+            )
+        )
+    }
+
     #[test]
     fn test_case_when() {
-        // TODO
+        test_parser!(
+            source = "
+                CASE
+                    WHEN 1 THEN 2
+                END
+            ",
+            parser = case_expr(),
+            expected = CaseExpr::new(
+                None,
+                vec![
+                    CaseWhen::new(
+                        ExprNode::IntegerConst(1),
+                        ExprNode::IntegerConst(2),
+                    ),
+                ],
+                None
+            )
+        )
     }
 }
 
@@ -50,7 +91,7 @@ use crate::parser::ast_node::CaseExpr;
 use crate::parser::ast_node::CaseWhen;
 use crate::parser::ast_node::ExprNode;
 use crate::parser::combinators::expr::a_expr;
-use crate::parser::combinators::foundation::many;
+use crate::parser::combinators::foundation::{and, many};
 use crate::parser::combinators::foundation::sequence;
 use crate::parser::combinators::foundation::Combinator;
 use crate::parser::combinators::foundation::CombinatorHelpers;
