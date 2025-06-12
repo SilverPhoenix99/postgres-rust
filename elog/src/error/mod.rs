6fc6627284976d6a4635d2301f6222be57dbc68a
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2025-06-13 00:33:34 +0100

    Renamed UnicodeStringError to unicode_string::Error.

--- elog/src/error/mod.rs
+++ elog/src/error/mod.rs
@@ -2,6 +2,7 @@ pub(super) mod extended_string;
 pub(super) mod lexer;
 pub(super) mod located_error;
 pub(super) mod role_spec;
+pub(super) mod unicode_string;
 
 pub trait Error: core::error::Error {
 
