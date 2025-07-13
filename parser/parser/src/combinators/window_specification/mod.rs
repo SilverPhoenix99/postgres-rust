2edc4751524e9f127ec3099de4e1c226f77a090f
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2025-07-13 15:51:43 +0100

    Added tests for window_specification.

--- parser/parser/src/combinators/window_specification/mod.rs
+++ parser/parser/src/combinators/window_specification/mod.rs
@@ -31,6 +31,68 @@ pub(super) fn window_specification(stream: &mut TokenStream) -> scan::Result<Win
     Ok(expr)
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::test_parser;
+    use pg_ast::ExprNode;
+    #[allow(unused_imports)]
+    use pg_ast::ExprNode::IntegerConst;
+    use pg_ast::FrameExtent;
+    use pg_ast::SortBy;
+    use pg_ast::WindowExclusion;
+    use pg_ast::WindowFrame;
+    use pg_ast::WindowFrameKind;
+    use pg_basics::Str;
+    use test_case::test_case;
+
+    #[test_case("(foo partition by 1 order by 2 range current row)", Some("foo".into()), some_partition(), some_order(), some_frame())]
+    #[test_case("(foo partition by 1 order by 2)",                   Some("foo".into()), some_partition(), some_order(), None)]
+    #[test_case("(foo partition by 1 range current row)",            Some("foo".into()), some_partition(), None,         some_frame())]
+    #[test_case("(foo partition by 1)",                              Some("foo".into()), some_partition(), None,         None)]
+    #[test_case("(foo order by 2 range current row)",                Some("foo".into()), None,             some_order(), some_frame())]
+    #[test_case("(foo order by 2)",                                  Some("foo".into()), None,             some_order(), None)]
+    #[test_case("(foo range current row)",                           Some("foo".into()), None,             None,         some_frame())]
+    #[test_case("(foo)",                                             Some("foo".into()), None,             None,         None)]
+    #[test_case("(partition by 1 order by 2 range current row)",     None,               some_partition(), some_order(), some_frame())]
+    #[test_case("(partition by 1 order by 2)",                       None,               some_partition(), some_order(), None)]
+    #[test_case("(partition by 1 range current row)",                None,               some_partition(), None,         some_frame())]
+    #[test_case("(partition by 1)",                                  None,               some_partition(), None,         None)]
+    #[test_case("(order by 2 range current row)",                    None,               None,             some_order(), some_frame())]
+    #[test_case("(order by 2)",                                      None,               None,             some_order(), None)]
+    #[test_case("(range current row)",                               None,               None,             None,         some_frame())]
+    #[test_case("()",                                                None,               None,             None,         None)]
+    fn test_window_specification(
+        source: &str,
+        name: Option<Str>,
+        partition: Option<Vec<ExprNode>>,
+        order: Option<Vec<SortBy>>,
+        frame: Option<WindowFrame>
+    ) {
+        test_parser!(
+            source,
+            window_specification,
+            WindowDefinition::new(name, partition, order, frame)
+        )
+    }
+
+    fn some_partition() -> Option<Vec<ExprNode>> {
+        Some(vec![IntegerConst(1)])
+    }
+
+    fn some_order() -> Option<Vec<SortBy>> {
+        Some(vec![SortBy::new(IntegerConst(2), None, None)])
+    }
+
+    fn some_frame() -> Option<WindowFrame> {
+        Some(WindowFrame::new(
+            WindowFrameKind::Range,
+            FrameExtent::CurrentRow { end: None },
+            WindowExclusion::default()
+        ))
+    }
+}
+
 #[allow(unused_imports)]
 use self::{
     existing_window_name::existing_window_name,
