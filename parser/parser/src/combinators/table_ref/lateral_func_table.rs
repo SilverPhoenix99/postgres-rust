2daf7c980d1a31ce9af03053b7d80dee0c389cca
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2025-08-04 23:43:38 +0100

    Implemented ordinality.

---
+++ parser/parser/src/combinators/table_ref/lateral_func_table.rs
@@ -0,0 +1,30 @@
+/// Alias: `opt_ordinality`
+fn ordinality(stream: &mut TokenStream) -> scan::Result<()> {
+
+    /*
+        WITH ORDINALITY
+    */
+
+    seq!(With, Ordinality).parse(stream)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::test_parser;
+    use test_case::test_case;
+
+    #[test_case("with ordinality" => Ok(()))]
+    fn test_ordinality(source: &str) -> scan::Result<()> {
+        test_parser!(source, ordinality)
+    }
+}
+
+use crate::combinators::foundation::seq;
+use crate::combinators::foundation::Combinator;
+use crate::scan;
+use crate::stream::TokenStream;
+use pg_lexer::Keyword::Ordinality;
+use pg_lexer::Keyword::With;
--- parser/parser/src/combinators/table_ref/mod.rs
+++ parser/parser/src/combinators/table_ref/mod.rs
@@ -2,6 +2,7 @@ pg_basics::reexport! {
     alias_clause,
     func_alias_clause,
     json_table,
+    lateral_func_table,
     rowsfrom_list,
     xmltable,
 }
