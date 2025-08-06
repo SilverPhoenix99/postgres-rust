3e652cdca8e265edd378468867f98faf792968c5
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2025-08-06 16:27:09 +0100

    Simplified test_parser!().

--- parser/parser/src/tests.rs
+++ parser/parser/src/tests.rs
@@ -2,23 +2,6 @@
 
 macro_rules! test_parser {
 
-    (internal =>
-        $source:expr,
-        $parser:expr,
-        $expected:expr
-    ) => {{
-        let actual = test_parser!($source, $parser);
-        let expected = $expected;
-        assert_eq!(expected, actual);
-    }};
-
-    ($source:expr, $parser:expr) => {{
-        let source = $source;
-        let mut stream = pg_parser_core::stream::TokenStream::from(source);
-        let parser = $parser;
-        $crate::combinators::foundation::Combinator::parse(&parser, &mut stream)
-    }};
-
     (
         source = $source:expr,
         parser = $parser:expr
@@ -29,50 +12,27 @@ macro_rules! test_parser {
     (
         source = $source:expr,
         parser = $parser:expr,
-        expected = Err($expected:expr)
-    ) => {
-        test_parser!(internal => $source, $parser, Err($expected.into()))
-    };
-
-    (
-        source = $source:expr,
-        parser = $parser:expr,
-        expected = Ok($expected:expr)
-    ) => {
-        test_parser!(internal => $source, $parser, Ok($expected.into()))
-    };
-
-    (
-        source = $source:expr,
-        parser = $parser:expr,
         expected = $expected:expr
     ) => {
-        test_parser!(internal => $source, $parser, Ok($expected.into()))
+        test_parser!($source, $parser, $expected)
     };
 
-    (
-        $source:expr,
-        $parser:expr,
-        Err($expected:expr)
-    ) => {
-        test_parser!(internal => $source, $parser, Err($expected.into()))
-    };
-
-    (
-        $source:expr,
-        $parser:expr,
-        Ok($expected:expr)
-    ) => {
-        test_parser!(internal => $source, $parser, Ok($expected.into()))
-    };
+    ($source:expr, $parser:expr) => {{
+        let source = $source;
+        let mut stream = pg_parser_core::stream::TokenStream::from(source);
+        let parser = $parser;
+        $crate::combinators::foundation::Combinator::parse(&parser, &mut stream)
+    }};
 
     (
         $source:expr,
         $parser:expr,
         $expected:expr
-    ) => {
-        test_parser!(internal => $source, $parser, Ok($expected.into()))
-    };
+    ) => {{
+        let actual = test_parser!($source, $parser);
+        let expected = $expected;
+        assert_eq!(Ok(expected.into()), actual);
+    }};
 }
 
 pub(crate) use test_parser;
