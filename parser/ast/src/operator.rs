8857a79fde1e01489d94f16dc0c2fab426d06f44
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2026-04-14 20:43:00 +0100

    Document operators.

--- parser/ast/src/operator.rs
+++ parser/ast/src/operator.rs
@@ -1,22 +1,40 @@
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Operator {
+    /// `+`
     Addition,
+    /// `-`
     Subtraction,
+    /// `*`
     Multiplication,
+    /// `/`
     Division,
+    /// `%`
     Modulo,
+    /// `^`
     Exponentiation,
+    /// `<`
     Less,
+    /// `>`
     Greater,
+    /// `=`
     Equals,
+    /// `<=`
     LessEquals,
+    /// `>=`
     GreaterEquals,
+    /// `!=` or `<>`
     NotEquals,
+    /// `LIKE`
     Like,
+    /// `NOT LIKE`
     NotLike,
+    /// `ILIKE`
     ILike,
+    /// `NOT ILIKE`
     NotILike,
+    /// `->`
     RightArrow,
+    /// `|`
     Pipe,
     UserDefined(Box<str>),
 }
