59c2c2365946943aaeb556742bc41e04359783bc
SilverPhoenix99 <antoniopedrosilvapinto+github@gmail.com>
2026-04-21 23:28:12 +0100

    Implemented select_stmt for array_expr.

--- parser/parser/src/combinators/expr/expr_primary/array_expr.rs
+++ parser/parser/src/combinators/expr/expr_primary/array_expr.rs
@@ -1,11 +1,21 @@
 pub(super) fn array_expr(ctx: &mut ParserContext) -> scan::Result<ExprNode> {
 
     /*
-        ARRAY '[' ( array_expr_list | expr_list )? ']'
+        ARRAY (
+              '[' ( array_expr_list | expr_list )? ']'
+            | '(' select_stmt ')'
+        )
     */
 
-    let (_, array) = seq!(Kw::Array, array_expr_inner).parse(ctx)?;
-    Ok(Array(array))
+    let (_, expr) = seq!(
+        Kw::Array,
+        alt!(
+            array_expr_inner.map(Array),
+            paren!(select_stmt).map(From::from)
+        )
+    ).parse(ctx)?;
+
+    Ok(expr)
 }
 
 /// The `Option` result does not come from an absence of value.
@@ -43,6 +53,7 @@ mod tests {
 
     #[test_case("array[]" => Ok(Array(None)); "empty array")]
     #[test_case("array[1]" => Ok(Array(Some(vec![IntegerConst(1)]))))]
+    #[test_case("array(select1)" => ignore["select_stmt not implemented yet"] matches Ok(_))]
     fn test_array_expr(source: &str) -> scan::Result<ExprNode> {
         test_parser!(source, array_expr)
     }
@@ -65,8 +76,10 @@ use crate::alt;
 use crate::brackets;
 use crate::combinators::core::Combinator;
 use crate::combinators::expr_list;
+use crate::combinators::stmt::select_stmt;
 use crate::context::ParserContext;
 use crate::many;
+use crate::paren;
 use crate::seq;
 use pg_ast::ExprNode;
 use pg_lexer::Keyword as Kw;
